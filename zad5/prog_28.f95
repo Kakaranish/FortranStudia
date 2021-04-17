04d8c43ad1008bc09dda783978a0e529b11d3a0a
Kakaranish <stasiekgruz@gmail.com>
2021-04-17 20:50:49 +0200

    [ZAD 5] + added progs for corresponding ZAD

---
+++ zad5/prog_28.f95
@@ -0,0 +1,10 @@
+PROGRAM p28
+    IMPLICIT NONE
+    CHARACTER(LEN=6) :: a, b, c, d
+    PRINT *, 'Type string 123456789'
+    READ '(A8,T1,A4,T7, A6, T4, A)', a, b, c, d
+
+    PRINT '(5X,A8,5X,A4,A,5X,A6)', a, b, c, d
+    PRINT '(5X,A,5X,A,5X,A,5X,A1)', a, b, c, d
+STOP
+END PROGRAM p28
